bfb0eafb19b440d6934a29cb32c4a4ed2b7ef197
JoelWaterworth <joelwaterworth@gmail.com>
2017-12-30 14:13:31 +0000

    reference counting is implemented with Rc<RefMut<Data>>, however there is no clone

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -1,47 +1,50 @@
 use type_checker::typed_ast::*;
 use parsing::ast::{BinaryOperator, Lit};
 use std::collections::HashMap;
-use std::borrow::Borrow;
+use std::cell::RefCell;
 use std::rc::Rc;
-use std::cell::Cell;
+use std::ops::Deref;
+use std::ops::DerefMut;
 
-type Ref<T>  = Rc<Cell<T>>;
-type RData = Ref<RData>;
+type RData = Rc<RefCell<Data>>;
+
+fn rdata(data: Data) -> RData {
+    Rc::new(RefCell::new(data))
+}
 
 #[derive(Debug, Clone)]
 enum Data {
     Int(u64),
     Closure(Function),
-    Method(Box<Data>, String),
+    Method(RData, String),
     Unit,
     Bool(bool),
-    StructVal(String, HashMap<String, Data>, HashMap<String, Data>), //name, fields, method table
-    Class(String, HashMap<String, Data>), //name, fields
+    Class(String, HashMap<String, RData>), //name, fields
 }
 
 #[derive(Clone, Debug)]
 struct Environment {
-    vars: HashMap<String, Data>,
+    vars: HashMap<String, RData>,
 }
 
 pub fn interpret_source_file(tast: Globals) {
-    let mut globals:HashMap<String, Data> = HashMap::new();
-    let mut types:HashMap<String,HashMap<String,Data>> = HashMap::new();
+    let mut globals:HashMap<String, RData> = HashMap::new();
+    let mut types:HashMap<String,HashMap<String,RData>> = HashMap::new();
 
     for s in tast.structs.into_iter() {
-        let args_data: HashMap<String,Data> =  s.args.iter().map(|(name, _)| {
-            (name.clone(), Data::Unit)
+        let args_data: HashMap<String,RData> =  s.args.iter().map(|(name, _)| {
+            (name.clone(), rdata(Data::Unit))
         }).collect();
 
-        let functions:HashMap<String,Data> = s.methods.into_iter().map(|(name, f)| {
-            (name, Data::Closure(f) )
+        let functions:HashMap<String,RData> = s.methods.into_iter().map(|(name, f)| {
+            (name, rdata(Data::Closure(f)) )
         }).collect();
 
         types.insert(s.name.clone(), functions);
     }
 
     for f in tast.functions.iter() {
-        globals.insert(f.name.clone(), Data::Closure(f.clone()));
+        globals.insert(f.name.clone(), rdata(Data::Closure(f.clone())));
     }
 
     let main_func = globals.get("main").unwrap().clone();
@@ -51,19 +54,19 @@ pub fn interpret_source_file(tast: Globals) {
 
 #[derive(Clone, Debug)]
 struct Executor {
-    globals: HashMap<String,Data>,
-    types: HashMap<String,HashMap<String,Data>>,
+    globals: HashMap<String,RData>,
+    types: HashMap<String,HashMap<String,RData>>,
 }
 
 impl Executor {
-    fn apply(&self, func: &Data, args: Vec<Data>) -> Data {
-        match func {
-            &Data::Closure(ref func_def) => {
+    fn apply(&self, func: &RData, args: Vec<RData>) -> RData {
+        match *(func).borrow() {
+            Data::Closure(ref func_def) => {
                 match func_def.cases {
                     Body::BuiltInFunc(ref name) => {
                         if *name == "print" {
                             println!("{:?}", args);
-                            return Data::Unit
+                            return rdata(Data::Unit)
                         } else {
                             panic!("Unknown builtin")
                         }
@@ -80,7 +83,7 @@ impl Executor {
                                             None => {}
                                         }
                                     }
-                                    return Data::Unit
+                                    return rdata(Data::Unit)
                                 },
                                 None => {}
                             }
@@ -89,8 +92,8 @@ impl Executor {
                 }
                 panic!("No matching function cases {:?}", func_def)
             },
-            &Data::Method(ref var, ref ty) => {
-                match var.borrow() {
+            Data::Method(ref var, ref ty) => {
+                match var.borrow().deref() {
                     &Data::Class(ref name, ref fields) => {
                         let funcs = self.types.get(name).unwrap();
                         match fields.get(ty) {
@@ -112,27 +115,31 @@ impl Executor {
         }
     }
 
-    fn eval(&self, env: &Environment, expr: &Expr) -> Data {
+    fn eval(&self, env: &Environment, expr: &Expr) -> RData {
         match expr {
             &Expr::BinaryExpr(ref op, ref left, ref right) => {
-                let l = self.eval(env, left);
-                let r = self.eval(env, right);
+                let rl = self.eval(env, left);
+                let rr = self.eval(env, right);
+                let rrl = rl.borrow();
+                let rrr = rr.borrow();
+                let l = rrl.deref();
+                let r = rrr.deref();
                 match op {
                     &BinaryOperator::Add => {
                         match (l, r) {
-                            (Data::Int(x), Data::Int(y)) => return Data::Int(x+y),
+                            (&Data::Int(x), &Data::Int(y)) => return rdata(Data::Int(x+y)),
                             (x, y) => panic!("type error with {:?}, {:?}", x, y)
                         }
                     }
                     &BinaryOperator::Sub => {
                         match (l, r) {
-                            (Data::Int(x), Data::Int(y)) => return Data::Int(x-y),
+                            (&Data::Int(x), &Data::Int(y)) => return rdata(Data::Int(x-y)),
                             _ => panic!("type error")
                         }
                     }
                     &BinaryOperator::IsEqualTo => {
                         match (l, r) {
-                            (Data::Int(x), Data::Int(y)) => return Data::Bool(x == y),
+                            (&Data::Int(x), &Data::Int(y)) => return rdata(Data::Bool(x == y)),
                             _ => panic!("type error")
                         }
                     }
@@ -152,7 +159,7 @@ impl Executor {
                     }
                 }
             }
-            &Expr::Lit(Lit::Integral(n)) => Data::Int(n),
+            &Expr::Lit(Lit::Integral(n)) => rdata(Data::Int(n)),
             &Expr::App(ref func, ref args) => {
                 let func_data = self.eval(env, func);
                 let args_data = args.iter().map(|x|{
@@ -167,7 +174,7 @@ impl Executor {
                         for &(ref name, ref val) in args.iter() {
                             fields.insert(name.clone(), self.eval(env, &val));
                         };
-                        Data::Class(x.clone(), fields)
+                        rdata(Data::Class(x.clone(), fields))
                     },
                     _ => panic!("")
                 }
@@ -177,13 +184,13 @@ impl Executor {
                 let args_data = args.iter().map(|x|{
                     self.eval(env, x)
                 }).collect();
-                self.apply(&Data::Method(Box::new(var), field.clone()), args_data)
+                self.apply(&rdata(Data::Method(var, field.clone())), args_data)
             }
             _ => panic!("not implemented {:?}", expr)
         }
     }
 
-    fn exec(&self, env: &mut Environment, statement: &Statement) -> Option<Data> {
+    fn exec(&self, env: &mut Environment, statement: &Statement) -> Option<RData> {
         match statement {
             &Statement::FunctionCall(ref func, ref args) => {
                 let func_data = self.eval(env, func);
@@ -198,7 +205,8 @@ impl Executor {
             }
             &Statement::If{ref condition, ref true_statements, ref false_statements} => {
                 let x = self.eval(env, condition);
-                match x {
+                let rx = x.borrow();
+                match *rx {
                     Data::Bool(true) => {
                         for statement in true_statements.iter() {
                             match self.exec(env, statement) {
@@ -227,24 +235,28 @@ impl Executor {
                 let args_data = args.iter().map(|x|{
                     self.eval(env, x)
                 }).collect();
-                self.apply(&Data::Method(Box::new(var), field.clone()), args_data);
+                self.apply(&rdata(Data::Method(var, field.clone())), args_data);
             }
             &Statement::Assignment{ref l_expr, ref expr} => {
                 let x = self.eval(env, expr);
-                *self.eval_l_expr(l_expr, env) = x;
+                let y = self.eval_l_expr(l_expr, env);
+                *y.borrow_mut() = x.borrow().deref().clone();
             }
             x => panic!("not implemented {:?}", x)
         }
         return None
     }
 
-    fn eval_l_expr<'a>(&self, l_expr: &LExpr, env: &'a mut Environment) -> &'a mut Data {
+    fn eval_l_expr<'a>(&self, l_expr: &LExpr, env: &'a Environment) -> RData {
         match l_expr {
-            &LExpr::Var(ref name) => env.vars.get_mut(name).unwrap(),
+            &LExpr::Var(ref name) => env.vars.get(name).unwrap().clone(),
             &LExpr::MethodCall(ref var, ref field, _) => {
-                match self.eval_l_expr(var, env) {
-                    &mut Data::Class(ref name, ref mut fields) => {
-                        fields.get_mut(field).unwrap()
+                let mut l = self.eval_l_expr(var, env).clone();
+                let mut rl = l.borrow();
+                let mut rrl = rl.deref();
+                match rrl {
+                    &Data::Class(ref name, ref fields) => {
+                        fields.get(field).unwrap().clone()
                     }
                     x => panic!("{:?}", x)
                 }
@@ -253,7 +265,7 @@ impl Executor {
         }
     }
 
-    fn func_match<'a>(&self, case: &'a Case, args: &Vec<Data>) -> Option<(&'a Vec<Statement>, Environment)> {
+    fn func_match<'a>(&self, case: &'a Case, args: &Vec<RData>) -> Option<(&'a Vec<Statement>, Environment)> {
         let mut env = Environment{vars: HashMap::new() };
         if case.args.is_empty() {
             return Some((&case.statements, env))
@@ -266,14 +278,14 @@ impl Executor {
         return Some((&case.statements, env))
     }
 
-    fn func_match_arg_check(&self, parameter: &Pattern, arg: &Data, env: &mut Environment) -> bool {
+    fn func_match_arg_check(&self, parameter: &Pattern, arg: &RData, env: &mut Environment) -> bool {
         match parameter {
             &Pattern::Binding{ref name, ref ty } => {
                 env.vars.insert(name.clone(), arg.clone());
                 return true
             },
             &Pattern::Constant {value: ConstVal{ref ty, val: Lit::Integral(ref n)}} => {
-                match arg {
+                match arg.borrow().deref() {
                     &Data::Int(ref m) => if n == m {
                         return true
                     } else {
